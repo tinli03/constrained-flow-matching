b5a68d4e90dc1387b20d1efc58181e8ef9b18df0
Rose Li <roseli06@MacBook-Pro-som-tillhor-Rose.local>
2026-07-22 13:42:12 +0800

    everything in funcions

--- source_to_file.py
+++ source_to_file.py
@@ -1,23 +1,24 @@
 import csv
 import numpy as np
+import pandas as pd 
 
 
 
-def source(n): # one list/vector with 10 dimensions from dirichlet distribution
-    alpha_source = np.ones(n)
+def source(number_of_dim): # one list/vector with 10 dimensions from dirichlet distribution
+    alpha_source = np.ones(number_of_dim)
     x0 = np.random.dirichlet(alpha_source, size=1)
     return x0
 
+def n_dims():
+    data = pd.read_csv("source_samples.csv", header=None)
+    x1 = data.iloc[0].tolist()
+    n_dims = len(x1)
+    return n_dims
 
+def create_source(number_of_lists):
+    with open("source_samples.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        for _ in range(number_of_lists):
+            one_list = source(10)
+            writer.writerows(one_list)
 
-number_of_lists = 12
-
-with open("source_samples.csv", "w", newline="") as file:
-
-    writer = csv.writer(file)
-
-    for _ in range(number_of_lists):
-
-        one_list = source(10)
-
-        writer.writerows(one_list)
