1ec726898794d3a315097907a13375a7507ffcf3
Rose Li <roseli06@MacBook-Pro-som-tillhor-Rose.local>
2026-07-23 17:02:36 +0800

    added functions to 1. create csv file for the source vectors 2. read the csv file and make a tensor 3. input: tensor of source vectors. output in terminal: generated/final step from source vectors 4. create csv file for the final vectors

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 # inferring using trained model
 import torch
+import csv
 import numpy as np
 from input import sample_training_batch
 from model import FlowNetwork
@@ -8,17 +9,45 @@ from utils import to_tensor
 import matplotlib.pyplot as plt
 from train import parse_args
 from input import sample_training_batch 
+from source_to_file import source
 
 args = parse_args()
 model = FlowNetwork(args, N_DIM)
 model.load_state_dict(torch.load(args.checkpoint_path))
 
 
+def tensor_to_csv(tensor_matrix): # ger ut i CSV alla slutpunkter från data.csv
+    list = tensor_matrix.tolist()
+    with open("unconstrained_generated.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        for n in range(len(list)):
+            one_list = list[n]
+            writer.writerow(one_list)
 
-def sample_unconstrained(model, batch_size, n_steps):
-    xt, _, _ = sample_training_batch(batch_size) 
-    dt = 1.0 / n_steps # hur lång tid varje steg är
-    x = to_tensor(xt)
+
+def tensor_from_source(): # läser av en csv och gör en tensor för att användas i genereringen av samples
+    list = []
+    data = np.loadtxt("data.csv", delimiter=",")
+    for n in range(data.shape[0]):
+        list.append(data[n])
+    source_matrix = np.array(list)
+    source_tensor = to_tensor(source_matrix)
+
+    return source_tensor
+
+def create_csv_source(number_of_sources): # skapar en csv med alla fasta, source punkter som används vid evaluation för alla metoder
+    with open("data.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        for _ in range(number_of_sources):
+            one_list = source(10)
+            writer.writerow(one_list)
+
+
+
+
+def sample_unconstrained(model, n_steps): # ger ut i TERMINALEN alla slutpunkter från data.csv
+    dt = 1.0 / n_steps 
+    x = tensor_from_source()
     model.eval()
 
     with torch.no_grad():
@@ -26,6 +55,10 @@ def sample_unconstrained(model, batch_size, n_steps):
             t = torch.full((x.shape[0],), k * dt, dtype=x.dtype,device=x.device) # hur långt tidsmässigt vi har kommit fram 
             v = model(x, t)
             x += dt * v
+
     return x
 
-print(sample_unconstrained(model, 5, 5))
+
+generated = sample_unconstrained(model, 5)
+print(generated)
+print(tensor_to_csv(generated))
