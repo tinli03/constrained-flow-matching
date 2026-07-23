ab19c08034600ad739c5aade5ff65ca245c28401
Rose Li <roseli06@MacBook-Pro-som-tillhor-Rose.local>
2026-07-23 21:36:20 +0800

    adjusted so that filename and method name are not fixed when running the functions.

--- sample.py
+++ sample.py
@@ -16,9 +16,9 @@ model = FlowNetwork(args, N_DIM)
 model.load_state_dict(torch.load(args.checkpoint_path))
 
 
-def tensor_to_csv(tensor_matrix, number_of_steps): # ger ut i CSV alla slutpunkter från data.csv
+def tensor_to_csv(tensor_matrix, number_of_steps, method_name): # ger ut i CSV alla slutpunkter från data.csv
     list = tensor_matrix.tolist()
-    filename = f"{number_of_steps}steps_unconstrained_generated.csv"
+    filename = f"{number_of_steps}steps_{method_name}_generated.csv"
     with open(filename, "w", newline="") as file:
         writer = csv.writer(file)
         for n in range(len(list)):
@@ -26,9 +26,9 @@ def tensor_to_csv(tensor_matrix, number_of_steps): # ger ut i CSV alla slutpunkt
             writer.writerow(one_list)
 
 
-def tensor_from_source(): # läser av en csv och gör en tensor för att användas i genereringen av samples
+def tensor_from_source(filename): # läser av en csv och gör en tensor för att användas i genereringen av samples
     list = []
-    data = np.loadtxt("data.csv", delimiter=",")
+    data = np.loadtxt(filename, delimiter=",")
     for n in range(data.shape[0]):
         list.append(data[n])
     source_matrix = np.array(list)
@@ -46,9 +46,9 @@ def create_csv_source(number_of_sources): # skapar en csv med alla fasta, source
 
 
 
-def sample_unconstrained(model, n_steps): # ger ut i TERMINALEN alla slutpunkter från data.csv
+def sample_unconstrained(model, n_steps, filename): # ger ut i TERMINALEN alla slutpunkter från data.csv
     dt = 1.0 / n_steps 
-    x = tensor_from_source()
+    x = tensor_from_source(filename)
     model.eval()
 
     with torch.no_grad():
@@ -60,9 +60,14 @@ def sample_unconstrained(model, n_steps): # ger ut i TERMINALEN alla slutpunkter
     return x
 
 
+
+filename = "data.csv"
+method_name = "unconstrained"
+
+
 number_of_steps = 100
-generated = sample_unconstrained(model, number_of_steps)
-print(tensor_to_csv(generated, number_of_steps))
+generated = sample_unconstrained(model, number_of_steps, filename)
+print(tensor_to_csv(generated, number_of_steps, method_name))
 
 
 
