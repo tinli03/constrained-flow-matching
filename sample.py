e97adb7b7485e4cc54b50d3d857068425ae62438
Rose Li <roseli06@MacBook-Pro-som-tillhor-Rose.local>
2026-07-23 16:18:28 +0800

    completed the sample unconstrained function. Output: the last step for each start vector when unconstrained generated.

--- sample.py
+++ sample.py
@@ -6,16 +6,26 @@ from model import FlowNetwork
 from source_to_file import N_DIM 
 from utils import to_tensor
 import matplotlib.pyplot as plt
+from train import parse_args
+from input import sample_training_batch 
 
+args = parse_args()
 model = FlowNetwork(args, N_DIM)
 model.load_state_dict(torch.load(args.checkpoint_path))
-model.eval()
 
-def sample_unconstrained(model, x0, n_steps=100):
-    x = x0
+
+
+def sample_unconstrained(model, batch_size, n_steps):
+    xt, _, _ = sample_training_batch(batch_size) 
     dt = 1.0 / n_steps # hur lång tid varje steg är
-    for k in range(n_steps):
-        t = torch.full((x.shape[0],), k * dt) # hur långt tidsmässigt vi har kommit fram
-        v = model(x, t)
-        x = x + dt * v
+    x = to_tensor(xt)
+    model.eval()
+
+    with torch.no_grad():
+        for k in range(n_steps):
+            t = torch.full((x.shape[0],), k * dt, dtype=x.dtype,device=x.device) # hur långt tidsmässigt vi har kommit fram 
+            v = model(x, t)
+            x += dt * v
     return x
+
+print(sample_unconstrained(model, 5, 5))
