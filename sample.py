b2dd2dad9e4c9242e47d712e1e56a0f2a205cd50
Rose Li <roseli06@MacBook-Pro-som-tillhor-Rose.local>
2026-07-24 18:19:44 +0800

    added projection function. it takes a vector and gives the projected, feasible vector as the output

--- sample.py
+++ sample.py
@@ -36,13 +36,37 @@ def tensor_from_source(filename): # läser av en csv och gör en tensor för att
 
     return source_tensor
 
-def create_csv_source(number_of_sources): # skapar en csv med alla fasta, source punkter som används vid evaluation för alla metoder
-    with open("data.csv", "w", newline="") as file:
+def create_csv_source(number_of_sources, filename): # skapar en csv med alla fasta, source punkter som används vid evaluation för alla metoder
+    with open(filename, "w", newline="") as file:
         writer = csv.writer(file)
         for _ in range(number_of_sources):
             one_list = source(10)
             writer.writerow(one_list)
 
+
+def projection(vector): # input is a list
+    x = 0
+    not_zero = 0
+    diff_sign = 1
+    for n in range(len(vector)):
+        if vector[n] < 0: # all negative values set to zero
+            vector[n] = 0
+        else:
+            x += vector[n]
+            not_zero += 1
+
+    diff = 1-x
+    if diff < 0: # so you know if you should add or subtract on the vector
+        diff_sign = -1
+
+    t = abs(diff) / not_zero
+    for n in range(len(vector)):
+        if vector[n] != 0:
+            if diff_sign < 1:
+                vector[n] = vector[n] - t
+            elif diff_sign > 1:
+                vector[n] = vector[n] + t
+    return vector
 
 
 
@@ -56,9 +80,22 @@ def sample_unconstrained(model, n_steps, filename): # ger ut i TERMINALEN alla s
             t = torch.full((x.shape[0],), k * dt, dtype=x.dtype,device=x.device) # hur långt tidsmässigt vi har kommit fram 
             v = model(x, t)
             x += dt * v
-
     return x
 
+
+def sample_finalproj(model, n_steps, filename):
+    dt = 1.0 / n_steps 
+    x = tensor_from_source(filename)
+    model.eval()
+
+    with torch.no_grad():
+        for k in range(n_steps):
+            t = torch.full((x.shape[0],), k * dt, dtype=x.dtype,device=x.device) # hur långt tidsmässigt vi har kommit fram 
+            v = model(x, t)
+            x += dt * v
+
+    x = projection(x) ##### it is a tensor now containing many vectors, so need to make it a list or numpy, then project 
+    return x
 
 
 filename = "data.csv"
@@ -68,6 +105,8 @@ method_name = "unconstrained"
 number_of_steps = 100
 generated = sample_unconstrained(model, number_of_steps, filename)
 print(tensor_to_csv(generated, number_of_steps, method_name))
+
+create_csv_source
 
 
 
